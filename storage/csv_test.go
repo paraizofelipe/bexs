70aaece258a552d2d44e30bf1db994e9aa190a5a
Paraizo <felipeparaizo@gmail.com>
2021-05-23 19:12:19 -0300

    adds test to operations in csv files

--- storage/csv_test.go
+++ storage/csv_test.go
@@ -1,6 +1,7 @@
 package storage
 
 import (
+	"strings"
 	"testing"
 )
 
@@ -18,6 +19,35 @@ func TestAppendFile(t *testing.T) {
 		csv := NewCSVStorage(file.Name())
 		if err := csv.AppendFile(line); err != nil {
 			t.Error(err)
+		}
+	})
+
+	t.Run("multiples inserts in file", func(t *testing.T) {
+		var (
+			lines = []string{"GRU,CDG,100", "XPT,YPT,00", "XXX,YYY,999"}
+		)
+
+		file, err := Create(t.TempDir(), "example", [][]string{})
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		csv := NewCSVStorage(file.Name())
+		for _, line := range lines {
+			if err := csv.AppendFile(line); err != nil {
+				t.Error(err)
+			}
+		}
+
+		insertLines, err := csv.Lines()
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		current := strings.Join(insertLines[len(insertLines)-1], ",")
+		expected := lines[len(lines)-1]
+		if current != expected {
+			t.Errorf("current: %s --> expected: %s", current, expected)
 		}
 	})
 }
